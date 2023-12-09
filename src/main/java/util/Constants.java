d75a93a17f4d3e0735d2850b55ed9ab53890ef9b
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-09 20:43:00 +0300

    refactor: Изменить формат даты

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -6,5 +6,5 @@ public final class Constants {
     private Constants() {
 
     }
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 }
