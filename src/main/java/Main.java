fcb48d55910f7d7b394ee46359afff5568635b05
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-10 16:44:15 +0300

    refactor: Изменить Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import configuration.FileOidConfiguration;
 import configuration.OidConfiguration;
+import service.FailAlarmVarBindProcessor;
 import service.SnmpTrapReceiver;
+import service.VarBindProcessor;
 import ui.AlarmWindow;
 import ui.UIWindow;
 
@@ -8,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         OidConfiguration configuration = new FileOidConfiguration();
         AlarmWindow ui = new UIWindow(configuration.getInputs().values());
-        SnmpTrapReceiver trapReceiver = new SnmpTrapReceiver(configuration, ui);
+        VarBindProcessor processor = new FailAlarmVarBindProcessor(ui);
+        new SnmpTrapReceiver(configuration, processor).run();
     }
 }
