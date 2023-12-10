81fe67afe0bbe2cea8ccec8a91fc526979841e47
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-10 16:45:56 +0300

    feat: Проверять на наличие FAIL

--- src/main/java/service/FailAlarmVarBindProcessor.java
+++ src/main/java/service/FailAlarmVarBindProcessor.java
@@ -17,7 +17,7 @@ public class FailAlarmVarBindProcessor implements VarBindProcessor {
     public void processVarBinds(List<? extends VariableBinding> varBinds, OidConfiguration conf) {
         String oidMessage = varBinds.get(7).toString();
         for (String oid : conf.getAlarmOids().keySet()) {
-            if (oidMessage.contains(oid)) {
+            if (oidMessage.contains(oid) && oidMessage.contains("FAIL")) {
                 for (String input : conf.getInputs().keySet()) {
                     if (oidMessage.startsWith(input, oid.length())) {
                         System.out.println(oid + " " + conf.getAlarmOids().get(oid));
