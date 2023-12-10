34ee740228c3d44fa3b0420b651563f00897d7be
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-10 10:27:40 +0300

    feat: Добавить alarmColor

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -1,10 +1,14 @@
 package util;
 
+import java.awt.*;
 import java.time.format.DateTimeFormatter;
 
 public final class Constants {
     private Constants() {
 
     }
+
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    public static final Color alarmColor = Color.RED;
 }
