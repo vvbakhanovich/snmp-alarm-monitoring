1c6e78e1e5da131c647fd2461bddaa8d75451626
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-12 16:48:18 +0300

    feat: Добавить OK_STATUS

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -10,6 +10,8 @@ public final class Constants {
     }
 
     public static final String FAIL_STATUS = "FAIL";
+    public static final String WARN_STATUS = "WARN";
+    public static final String OK_STATUS = "OK";
     public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     public static final Color alarmColor = Color.RED;
     public static final String CONFIG_DIR = new File(System.getProperty("user.dir"), "/config").toString();
