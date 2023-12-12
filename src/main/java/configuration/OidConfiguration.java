85bd238bd830199df3b93af2ed7751a26b857ead
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-12 10:18:59 +0300

    feat: Добавить метод getButtonNames(), getIps()

--- src/main/java/configuration/OidConfiguration.java
+++ src/main/java/configuration/OidConfiguration.java
@@ -1,9 +1,15 @@
 package configuration;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public interface OidConfiguration {
     Map<String, String> getAlarmOids();
 
-    Map<String, String> getInputs();
+    Map<String, String> getInputs(String ip);
+
+    Set<String> getIps();
+
+    List<String> getButtonNames();
 }
