7290f1f2f0142baff92839129b37b0b13f1a5e1a
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-12 17:42:47 +0300

    feat: Добавить OK_COLOR

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -12,7 +12,8 @@ public final class Constants {
     public static final String FAIL_STATUS = "FAIL";
     public static final String WARN_STATUS = "WARN";
     public static final String OK_STATUS = "OK";
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-    public static final Color alarmColor = Color.RED;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    public static final Color ALARM_COLOR = Color.RED;
+    public static final Color OK_COLOR = Color.LIGHT_GRAY;
     public static final String CONFIG_DIR = new File(System.getProperty("user.dir"), "/config").toString();
 }
