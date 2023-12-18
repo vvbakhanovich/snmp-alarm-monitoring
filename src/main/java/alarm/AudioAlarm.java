dff38207b21c2df0baabfa203c20aebb2a95b028
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-18 17:49:11 +0300

    feat: Добавить логирование ошибок

--- src/main/java/alarm/AudioAlarm.java
+++ src/main/java/alarm/AudioAlarm.java
@@ -1,11 +1,16 @@
 package alarm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
 
 public class AudioAlarm implements Alarm {
+    private static final Logger log = LoggerFactory.getLogger(AudioAlarm.class);
+
     private final Clip clip;
 
     public AudioAlarm(final String alarmPath) {
@@ -15,6 +20,7 @@ public class AudioAlarm implements Alarm {
             clip = AudioSystem.getClip();
             clip.open(stream);
         } catch (Exception e) {
+            log.error("Ошибка при чтении звукового файла. {}", e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -25,15 +31,18 @@ public class AudioAlarm implements Alarm {
             clip.setMicrosecondPosition(0);
             clip.start();
         } catch (Exception e) {
+            log.error("Ошибка при попытке проиграть звуковой файл. {}", e.getMessage());
             throw new RuntimeException(e);
         }
     }
+
     @Override
     public void stopAudio() {
         try {
             clip.stop();
         } catch (Exception e) {
-           throw new RuntimeException(e);
+            log.error("Ошибка при попытке остановить проигрывание звукового файла. {}", e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
